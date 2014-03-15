74be9fae9cdd984a0b7e5ddd14a73fb16d22c2fa
Carl Consing <cmconsing@gmail.com>
2014-03-15 13:56:42 -0700

    initial commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,12 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key is used for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+# You can use `rake secret` to generate a secure secret key.
+
+# Make sure your secret_key_base is kept private
+# if you're sharing your code publicly.
+Striveware::Application.config.secret_key_base = '1dd273ff270405a69c67948bf8d15cf192693260df569a9df58c9d09ae3a67e51ecac8735f177fc95dd71cedf7615952e3891dc16701ad9da30fd4d5edb0b38f'
---
+++ config/initializers/session_store.rb
@@ -0,0 +1,3 @@
+# Be sure to restart your server when you modify this file.
+
+Striveware::Application.config.session_store :cookie_store, key: '_striveware_session'
