4a8bc16f5a37d781df987f201e518f8bd78f6047
Carl Consing <cmconsing@gmail.com>
2014-03-16 23:31:57 -0700

    Added user id to goals

--- db/schema.rb
+++ db/schema.rb
@@ -11,13 +11,16 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20140317022331) do
+ActiveRecord::Schema.define(version: 20140317055412) do
 
   create_table "goals", force: true do |t|
     t.string   "title"
     t.datetime "created_at"
     t.datetime "updated_at"
+    t.integer  "user_id"
   end
+
+  add_index "goals", ["user_id"], name: "index_goals_on_user_id"
 
   create_table "users", force: true do |t|
     t.string   "email",                  default: "", null: false
